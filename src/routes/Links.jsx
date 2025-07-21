b500d30c8ca3548ad9d0ee8615644713d8dc3278
Yaz Feisst <159829958+yazzamuffiny@users.noreply.github.com>
2025-07-21 22:46:47 +1200

    Update Links.jsx

--- src/routes/Links.jsx
+++ src/routes/Links.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 
 import Home from "../pages/Home"
-import SinglePokemon from "../pages/SinglePokemon"
 
 const Links = () => {
   return (
     <Routes>
         <Route exact path="/" element={<Home/>}/>
-        <Route path="/pokemon" element={<SinglePokemon/>}/>
     </Routes>
   )
 }
